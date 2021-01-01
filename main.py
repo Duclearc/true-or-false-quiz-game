80fd803fc0e0fb525affe3ac212b27d2db489cf8
Duclearc <59476105+Duclearc@users.noreply.github.com>
2021-01-01 15:46:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_bank.append(Question(question['text'], question['answer']))
+
+game = QuizBrain(question_bank)
+print(game.questions_list[1].text)
+
+game.next_question()
---
+++ quiz_brain.py
@@ -0,0 +1,30 @@
+# TODO: create QuizBrain class
+class QuizBrain:
+    def __init__(self, questions_list):
+        # TODO: create attributes question_number and questions_list
+        self.question_number = 0
+        self.questions_list = questions_list
+
+    # TODO: create method next_question
+    def next_question(self):
+        # TODO: check if quiz has ended
+        continue_quiz = True
+        while self.question_number < len(self.questions_list) and continue_quiz:
+            if 0 < self.question_number:
+                print(f'CORRECT! You now have {self.question_number} points.')
+            current_question = self.questions_list[self.question_number]
+            # TODO: ask questions
+            # TODO: check if answer is correct
+            if input(f'True or False? {current_question.text}\n').title() == current_question.answer:
+                self.question_number += 1
+            else:
+                if input("OPS! Sorry, that was incorrect. Play again? Type 'Yes' or 'No'.\n").title() == 'Yes':
+                    self.question_number = 0
+                    self.next_question()
+                else:
+                    print('Goodbye! 👋')
+                    continue_quiz = False
+        if continue_quiz:
+            print('CONGRATULATIONS! You won this quiz! 🥳')
+            self.question_number = 0
+            continue_quiz = False
